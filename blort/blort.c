956bbe32351b5dcb12250780b172cf8cb31e28bd
Dan Bornstein <danfuzz@milk.com>
2013-11-13 10:56:45 -0800

    Flesh out the code.

--- blort/blort.c
+++ blort/blort.c
@@ -1,1 +1,17 @@
+/*
+ * Copyright 2013 the DL Example Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+ */
+
 void hook(const char *);
+
+// TODO: Maybe also use `__attribute((weak))`.
+static void init(void) __attribute__((constructor));
+static void init(void) {
+    hook("init() called inside blort.");
+}
+
+void run(void) {
+    hook("run() called inside blort.");
+}
--- host/main.c
+++ host/main.c
@@ -1,1 +1,37 @@
-void hook(const char *);
+/*
+ * Copyright 2013 the DL Example Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * This is the hook function that is made available to the dynamically
+ * loaded libraries.
+ */
+void hook(const char *str) {
+    printf("%s\n", str);
+}
+
+/**
+ * Loads and runs the named library.
+ */
+static void runLib(const char *fileName) {
+    fprintf(stderr, "Running %s...\n", fileName);
+}
+
+/**
+ * Loads and runs each of the libraries named on the command-line.
+ */
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Too few arguments.\n");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        runLib(argv[i]);
+    }
+}
