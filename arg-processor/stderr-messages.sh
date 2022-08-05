ba46343138989925a1f49e36f3ee8e44ddcf38e3
Dan Bornstein <danfuzz@milk.com>
2022-08-05 11:31:49 -0700

    Two bits of improvement:
    * Add command name "for free" on first message.
    * Add `--read`.

--- arg-processor/stderr-messages.sh
+++ arg-processor/stderr-messages.sh
@@ -10,6 +10,9 @@
 _stderr_cmdName="$(readlink -f "$0")" || return "$?"
 _stderr_cmdName="${_stderr_cmdName##*/}"
 
+# Has an error been emitted?
+_stderr_anyErrors=0
+
 # Whether error messages are enabled.
 _stderr_errorEnabled=1
 
@@ -21,13 +24,51 @@ _stderr_progressEnabled=0
 # Library functions
 #
 
-# Prints an error message to stderr, if such are enabled.
+# Prints an error message to stderr, if such are enabled. Use option `--no-name`
+# to suppress printing of the top-level command name on the first message. Use
+# `--read` to read messages from stdin.
 #
 # Note: Error messages are _enabled_ by default.
 function error-msg {
-    if (( _stderr_errorEnabled )); then
-        echo 1>&2 "$@"
+    if (( !_stderr_errorEnabled )); then
+        return
     fi
+
+    local msg="$*"
+    local name="$(( !_stderr_anyErrors ))"
+    local read=0
+
+    while [[ $1 =~ ^-- ]]; do
+        case "$1" in
+            --no-name)
+                name=0
+                ;;
+            --read)
+                read=1
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo 1>&2 "Unrecognized option: $1"
+                return 1
+                ;;
+        esac
+        shift
+    done
+
+    if (( read )); then
+        msg="$(cat)"
+    fi
+
+    if (( name )); then
+        msg="${_stderr_cmdName}: ${msg}"
+    fi
+
+    # `printf` to avoid option-parsing weirdness with `echo`
+    printf 1>&2 '%s\n' "${msg}"
+    _stderr_anyErrors=1
 }
 
 # Enables or disables error messages.
